b5c341904e50f3aa022275d501ca07aaae679000
Xu Weijiang <weijiangxu@microsoft.com>
2025-03-12 10:21:37 +0800

    save work

--- app/work_queue.py
+++ app/work_queue.py
@@ -2,9 +2,10 @@ from app.libs.redis_queue import RedisQueue
 import app.config as app_config
 
 
-def connect_queue(is_async: bool = False) -> RedisQueue:
+def connect_queue(is_async: bool = False, enable_log=False) -> RedisQueue:
     return RedisQueue(
         redis_uri=app_config.REDIS_URI,
         queue_name=app_config.REDIS_WORK_QUEUE_NAME,
+        socket_timeout=app_config.REDIS_SOCKET_TIMEOUT,
         is_async=is_async,
     )
